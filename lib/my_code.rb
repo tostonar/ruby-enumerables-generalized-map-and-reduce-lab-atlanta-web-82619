8a7e4597ac26d1a29bc03593947859738bc435f3
Andrea Toston <tostonar@gmail.com>
2019-07-25 14:52:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,12 +3,12 @@ def map(source_array, block)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( block(source_array[i]) ) 
+    new.push( block(source_array[i]) )
     i += 1
   end
   return new
 end
 
 def reduce
-  
+
 end
