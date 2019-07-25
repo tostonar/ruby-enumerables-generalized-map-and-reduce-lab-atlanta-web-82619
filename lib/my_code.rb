b9fb5e12bb60a614d62a8ad9c8d0589cc618b14d
Andrea Toston <tostonar@gmail.com>
2019-07-25 14:40:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,9 @@
 # Your Code Here
-def map(array, block)
+def map(source_array, block)
   new = []
   i = 0
-  while i < array.length do
-    new << block(array[i])
+  while i < source_array.length do
+    new.push( block(source_array[i]) ) 
     i += 1
   end
   return new
