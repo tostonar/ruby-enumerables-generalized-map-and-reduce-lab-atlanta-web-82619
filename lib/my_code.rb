2bf93cdbbcad35c5af3f89e4dbc40c156c5e84e0
Andrea Toston <tostonar@gmail.com>
2019-07-25 16:43:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+      new.push(yield(source_array[i]))
+    i += 1
+  end
+  return new
+end
+
+def reduce(source_array, accumulator = 0)
+  source_array.each do |n|
+    accumulator = yield(accumulator, n)
+  end
+  if accumulator == 0
+    return true
+  else
+    return accumulator
+  end
+end
