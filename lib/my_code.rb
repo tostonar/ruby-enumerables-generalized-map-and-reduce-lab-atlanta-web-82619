dc77caf2428d26d8d5a1b89f45655613e3186dc0
Andrea Toston <tostonar@gmail.com>
2019-07-25 15:24:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,12 +9,12 @@ def map(source_array)
   return new
 end
 
-def reduce(source_array)
-
+def reduce(source_array, starting_point = 0)
+  new = starting_point
   i = 0
   while i < source_array.length do
      return yield(source_array[i])
     i += 1
   end
-  
+
 end
